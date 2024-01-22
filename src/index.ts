b033f131e280ea7e60b7e5f3a7f5b9bcfc1ec24e
Kevin <kitten.studio.publish@gmail.com>
2024-01-22 21:23:22 +0900

    feat : is training coding test

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+function lengthOfLongestSubstring(s: string): number {
+	let start = 0,
+		maxLength = 0
+	const map: Record<string, number> = {}
+
+	for (let end = 0; end < s.length; end++) {
+		const currentChar = s[end]
+		if (map[currentChar] !== undefined && map[currentChar] >= start) {
+			start = map[currentChar] + 1
+		}
+		map[currentChar] = end
+		maxLength = Math.max(maxLength, end - start + 1)
+	}
+
+	return maxLength
+}
+
+console.log(lengthOfLongestSubstring("abcabcbb"))
+
+export {}
