dfc45a8da001c6370aa18bdf741914b80de53054
Kevin <kitten.studio.publish@gmail.com>
2024-02-01 22:49:01 +0900

    feat : is training coding test

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,24 @@
-console.log("hello world!")
+// https://leetcode.com/problems/sort-characters-by-frequency/description/
+
 export {}
+
+function frequencySort(s: string): string {
+	const frequencyMap = new Map<string, number>()
+	for (const char of s) {
+		frequencyMap.set(char, (frequencyMap.get(char) || 0) + 1)
+	}
+	const sortedArray = Array.from(s)
+	sortedArray.sort((a, b) => {
+		const freqA = frequencyMap.get(a)
+		const freqB = frequencyMap.get(b)
+		if (freqB !== freqA) {
+			return freqB! - freqA!
+		}
+
+		return a.localeCompare(b)
+	})
+
+	return sortedArray.join("")
+}
+
+console.log(frequencySort("Aabb"))
